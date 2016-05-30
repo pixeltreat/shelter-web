e192c4f64d49216b1f813cec7c094f8420bcebc1
Pixeltreat <thepixeltreat@gmail.com>
2016-05-30 00:02:05 -0600

    markup updates and code clean up

--- application/assets/js/src/app/app-ui-scripts.js
+++ application/assets/js/src/app/app-ui-scripts.js
@@ -7,30 +7,30 @@ $(document).ready(function (e) {
         $(this).toggleClass('is-alerts-visible');
     });
 
-    // submenu
-    $appHeader.on('click', '.admin', function (e) {
-		$(this).toggleClass('active');
-		$(this).find('.sub-nav').slideToggle();
-		$(this).find('.sub-nav').toggleClass('collapse');
-	});
-
-    $appHeader.on('mouseleave', '.admin', function (e) {
-		$(this).find('.sub-nav').slideUp();
-		$(this).find('.sub-nav').removeClass('collapse');
-		$($(this)).removeClass('active');
-    });
-
-    $appHeader.on('click', '.nav a', function (e) {
-		e.preventDefault();
-	});
+    // // submenu
+    // $appHeader.on('click', '.admin', function (e) {
+	// 	$(this).toggleClass('active');
+	// 	$(this).find('.sub-nav').slideToggle();
+	// 	$(this).find('.sub-nav').toggleClass('collapse');
+	// });
+    //
+    // $appHeader.on('mouseleave', '.admin', function (e) {
+	// 	$(this).find('.sub-nav').slideUp();
+	// 	$(this).find('.sub-nav').removeClass('collapse');
+	// 	$($(this)).removeClass('active');
+    // });
+    //
+    // $appHeader.on('click', '.nav a', function (e) {
+	// 	e.preventDefault();
+	// });
 
 
     /* mobile specific */
-    $appHeader.on('click', '.m-menu', function (e) {
-        // toggle navigation
-        $(this).toggleClass('active');
-        $('.main-nav').toggleClass('expand');
-    });
+    // $appHeader.on('click', '.m-menu', function (e) {
+    //     // toggle navigation
+    //     $(this).toggleClass('active');
+    //     $('.main-nav').toggleClass('expand');
+    // });
 
     /*Start of closing error window event*/
     $('.msg-container').on('click', '.close-btn', function (e) {
